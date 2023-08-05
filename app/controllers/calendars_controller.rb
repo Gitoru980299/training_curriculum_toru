4f335cd766bae7ec201b5a370d22cbeb8743df46
Gitoru980299 <tmatsumoto299@gmail.com>
2023-08-05 16:58:51 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,8 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    binding.pry
+    Calendars.create(plan_params)
     redirect_to action: :index
   end
 
